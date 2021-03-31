5f3dedb1a4a5bcc22291012b432aa38e53a0ef7b
Liam Baron <liam_baron@hotmail.co.uk>
2021-03-31 14:56:45 +0100

    Finally finished the pre-built airport table!!!

--- app/src/main/java/com/lbaron/flyingweather/metarData/MetarDatabase.kt
+++ app/src/main/java/com/lbaron/flyingweather/metarData/MetarDatabase.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database (entities = [Metar::class, Airport::class], version = 4, exportSchema = true) // Entities are our tables defined by the appropriate class
+
+@Database(entities = [Metar::class, Airport::class], version = 6, exportSchema = false) // Entities are our tables defined by the appropriate class
 abstract class MetarDatabase : RoomDatabase() {
 
     abstract fun metarDao(): MetarDao
@@ -17,22 +20,37 @@ abstract class MetarDatabase : RoomDatabase() {
          * Return and also sets the database instance
          */
         fun getDatabase(context: Context): MetarDatabase{
+
+            val migration1: Migration = object : Migration(5, 6) {
+                override fun migrate(database: SupportSQLiteDatabase) {
+                    // Since we didn't alter the table, there's nothing else to do here.
+                }
+            }
+            val migration2: Migration = object : Migration(4, 6) {
+                override fun migrate(database: SupportSQLiteDatabase) {
+                    // Since we didn't alter the table, there's nothing else to do here.
+                }
+            }
+
             val tempInstance = INSTANCE
             if (tempInstance != null){
                 return tempInstance
             }
             synchronized(this){ // Apparently synchronised protects it from being access by multiple threads
                 val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    MetarDatabase::class.java,
-                    "metar_database"
+                        context.applicationContext,
+                        MetarDatabase::class.java,
+                        "metar_database"
                 )
                         .createFromAsset("database/metar_database.db")
-                        .fallbackToDestructiveMigration()
+                        .addMigrations(migration1,migration2)
+                        //.fallbackToDestructiveMigration()
                         .build()
                 INSTANCE = instance
                 return instance
             }
         }
     }
+
+
 }
