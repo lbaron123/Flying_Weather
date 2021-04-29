b4546596f6b8a0ac26ce7e0bbf096eca65037d44
Liam Baron <liam_baron@hotmail.co.uk>
2021-04-29 11:15:26 +0100

    Calendar recyclerview now has a scrolling list of days

--- app/src/main/java/com/lbaron/flyingweather/CalendarViewFragment.kt
+++ app/src/main/java/com/lbaron/flyingweather/CalendarViewFragment.kt
@@ -1,29 +1,54 @@
 package com.lbaron.flyingweather
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
-import com.lbaron.flyingweather.utility.u
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.lbaron.flyingweather.adapters.CalendarItemAdapter
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatter.BASIC_ISO_DATE
+import java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME
+import kotlin.collections.ArrayList
+
 
 class CalendarViewFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
             return inflater.inflate(R.layout.calendar_view_layout, container, false)
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btn: Button = view.findViewById(R.id.btn)
-        btn.setOnClickListener {
-            context?.let { it1 -> u.l(it1, "Button Pressed") }
-            findNavController().navigate(R.id.action_titleFragment_to_mainContent)
+        setupRecyclerView()
+    }
+
+    /**
+     * Recyclerview for the days and their airports and weathers
+     */
+    private fun setupRecyclerView(){
+        val recyclerView = requireView().findViewById<RecyclerView>(R.id.calendar_recycler_view)
+        // Getting the days to put in recyclerview
+        // Belopw block generates an array list of 100 days in format - Tuesday 04 June
+        val dateTimes = ArrayList<String>()
+        val today = LocalDate.now().toString()
+        val formatter = DateTimeFormatter.ofPattern("EEEE dd MMM")
+        for (i in 0..100){
+            val day = LocalDate.parse(today).plusDays(i.toLong())
+            val dayFormatted = day.format(formatter)
+            dateTimes.add(dayFormatted)
         }
+
+        // Below are the three lines that actually set up the recycler view
+        val adapter = CalendarItemAdapter(dateTimes)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity().applicationContext) // Linear layout manager for a vertical scrolling list
+
+        recyclerView.setHasFixedSize(true)                              // This is a performance optimisation
     }
 }
--- app/src/main/java/com/lbaron/flyingweather/adapters/CalendarItemAdapter.kt
+++ app/src/main/java/com/lbaron/flyingweather/adapters/CalendarItemAdapter.kt
@@ -1,23 +1,20 @@
 package com.lbaron.flyingweather.adapters
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.lbaron.flyingweather.R
-import com.lbaron.flyingweather.databaseStuff.Metar
 
 /**
  * In agle brackets in RV.Adapter we have out MetarViewHolder defined as a nested class below
  * This shows the default Adapter what viewholder we have to use
  * Implemented onCreateViewHolder onBindViewHolder getItemCount by pressing CTRL-I inside the class
- * @param metarList list of our objects to go into the recyclerview - private because it only needs to be accessed in the class
+ * @param dayList list of our objects to go into the recyclerview - private because it only needs to be accessed in the class
  */
-class MetarItemAdapter(val deleteMetar: (Metar) -> Unit) : RecyclerView.Adapter<MetarItemAdapter.MetarViewHolder>() {
-
-    private var metarList = emptyList<Metar>()
-
+class CalendarItemAdapter(private var dayList: ArrayList<String>) : RecyclerView.Adapter<CalendarItemAdapter.DayViewHolder>() {
 
     /**
      * onCreateViewHolder is called by RV when it is time to create a new viewholder
@@ -26,54 +23,50 @@ class MetarItemAdapter(val deleteMetar: (Metar) -> Unit) : RecyclerView.Adapter<
      * @param parent is the recyclerView
      * @param viewType used if more than one type of view in the RV
      */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MetarViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayViewHolder {
         // LayoutInflator turns xml into the itemView of the required type
         // Parent is the recyclerview, context is the activity we are in
-
-        return MetarViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.general_weather_recyclerview_item, parent, false))
+        return DayViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.calendar_recyclerview_item, parent, false))
     }
 
     /**
      * Called over and over again as we scroll, so need to be careful of what code we put in here
      * Changes the data in a view
-     * @param holder One of our MetarViewHolders to be filled with data
+     * @param holder One of our DayViewHolders to be filled with data
      * @param position of the item in the list
      */
-    override fun onBindViewHolder(holder: MetarViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: DayViewHolder, position: Int) {
         // Want position of item in dataset - is what we display
-        // metarList from the class constructor - all the data
-        val currentItem = metarList[position]
+        // dayList from the class constructor - all the data
+        val currentItem = dayList[position]
         // holder contains references to the views as defined below
-        holder.tvAirportName.text = currentItem.station
-        holder.tvMetar.text = currentItem.metar
-        holder.tvMetar.setOnClickListener {
-            deleteMetar(currentItem)
+        holder.tvDay.text = currentItem
+        holder.tvDay.setOnClickListener {
+            // TODO - what happens on Click
         }
     }
 
     override fun getItemCount(): Int {
-        return metarList.size
+        return dayList.size
     }
 
     /**
      * Viewholder represents a single row in the list
-     *  One instance of MetarViewHolder contains one instance of layout + metadata about row eg position
+     *  One instance of DayViewHolder contains one instance of layout + metadata about row eg position
      *  It works by caching references to each view tv_xxx for example
      *  We will have as many viewholders as required to fill the screen plus a bit of margin
      * @param itemView is one instance of our row layout - it contains the references to our textviews
      */
-    class MetarViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        // imageView : ImageView = itemView.findViewById(R.id.iv_image)  <-- Syntax if the layout has a picture
-        val tvAirportName: TextView = itemView.findViewById(R.id.tv_airport_name)
-        val tvMetar : TextView = itemView.findViewById(R.id.tv_metar)
+    class DayViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val tvDay: TextView = itemView.findViewById(R.id.tv_day)
     }
 
     /**
-     * Takes a list of metars and sets them to the class's internal structure
+     * Takes a list of days and sets them to the class's internal structure
      * It then notifies any observers that the data has changed
      */
-    fun setData(metarListInput: List<Metar>){
-        this.metarList = metarListInput
+    fun setData(dayListInput: ArrayList<String>){
+        this.dayList = dayListInput
         notifyDataSetChanged()
     }
 }
